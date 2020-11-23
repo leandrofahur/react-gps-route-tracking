822c4d8e17581af035fdd73aabd4127485e81ced
leandrofahur <leandrofahur@gmail.com>
2020-11-22 23:34:04 -0800

    display map with marker

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,15 @@
 import "./App.css";
 
 import React from "react";
-import mapboxgl, { Map } from "mapbox-gl";
+import mapboxgl from "mapbox-gl";
 
 mapboxgl.accessToken =
   "pk.eyJ1IjoibGVhbmRyb2ZhaHVyIiwiYSI6ImNqemJpeHAzdzAwcXgzbW1ub2p5YXlxZWwifQ.0jquNUq8yTAfeiVV6SDVkg";
 
 class App extends React.Component {
   state = {
-    lgn: -122.656563,
-    lat: 49.103569,
+    lng: -74.005974,
+    lat: 40.712776,
     zoom: 10,
   };
 
@@ -17,20 +17,40 @@ class App extends React.Component {
     const map = new mapboxgl.Map({
       container: this.mapContainer,
       style: "mapbox://styles/mapbox/streets-v11",
-      center: [this.state.lgn, this.state.lat],
+      center: [this.state.lng, this.state.lat],
       zoom: this.state.zoom,
     });
 
-    const marker = new mapboxgl.Marker()
-      .setLngLat([this.state.lgn, this.state.lat])
+    const marker = new mapboxgl.Marker({
+      anchor: "left",
+      color: "red",
+      draggable: true,
+    })
+      .setLngLat([this.state.lng, this.state.lat])
       .addTo(map);
+
+    // let geolocate = new mapboxgl.GeolocateControl({
+    //   positionOptions: {
+    //     enableHighAccuracy: true,
+    //   },
+    //   trackUserLocation: true,
+    // });
+
+    // map.addControl(geolocate, "top-left");
+    // map.on("load", () => {
+    //   geolocate.trigger();
+    // });
+
+    // map.on("mousemove", (e) => {
+    //   console.log(JSON.stringify(e.lngLat.wrap()));
+    // });
   }
 
   render() {
     return (
       <div
-        ref={(elemet) => {
-          this.mapContainer = elemet;
+        ref={(element) => {
+          this.mapContainer = element;
         }}
         className="mapContainer"
       >
